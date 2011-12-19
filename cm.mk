58daffcac5b0771400ca20aa8330d587aa862d44
Ethan Chen <intervigil@gmail.com>
2011-12-18 17:26:27 -0800

    build id is IML74K

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pyramid BUILD_ID=ITL41D BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:2.3.4/GRJ22/125597.1:user/release-keys PRIVATE_BUILD_DESC="pyramid-user 2.3.4 GRJ22 125597.1 release-keys" BUILD_NUMBER=125597
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pyramid BUILD_ID=IML74K BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:2.3.4/GRJ22/125597.1:user/release-keys PRIVATE_BUILD_DESC="pyramid-user 2.3.4 GRJ22 125597.1 release-keys" BUILD_NUMBER=125597
 
 PRODUCT_NAME := cm_pyramid
 PRODUCT_DEVICE := pyramid
