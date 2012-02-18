407973cee56980cf69dec984c75eaa8f3cc391c7
Ethan Chen <intervigil@gmail.com>
2012-02-18 13:52:36 -0800

    update build fingerprint to match 4.0 leak

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pyramid BUILD_ID=IML74K BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:2.3.4/GRJ22/125597.1:user/release-keys PRIVATE_BUILD_DESC="pyramid-user 2.3.4 GRJ22 125597.1 release-keys" BUILD_NUMBER=125597
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_ID=IML74K BUILD_FINGERPRINT=htc_wwe/htc_pyramid/pyramid:4.0.3/IML74K/275847.101:user/release-keys PRIVATE_BUILD_DESC="3.25.401.101 CL275847 release-keys" BUILD_NUMBER=275847
 
 PRODUCT_NAME := cm_pyramid
 PRODUCT_DEVICE := pyramid
