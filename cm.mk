cb660b02b2cc7c84fddde5788738d53b21590b8e
vorbeth <vorbeth@gmail.com>
2012-03-17 01:46:33 +0000

    New boot animation from Cameron Behzadpour
    Change-Id: I0f87a72ec8bc3055dedff4446e52ed3bbe554cc4

--- cm.mk
+++ cm.mk
@@ -2,6 +2,8 @@ $(call inherit-product, device/htc/pyramid/full_pyramid.mk)
 
 PRODUCT_RELEASE_NAME := pyramid
 
+TARGET_BOOTANIMATION_NAME := vertical-540x960
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
